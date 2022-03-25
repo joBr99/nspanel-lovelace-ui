77b05b80799b56a97af8756143d5381e3d126912
Johannes <johannes+develop+github@braun-rheingau.de>
2022-03-25 15:46:03 +0100

    add min brightness

--- apps/nspanel-lovelace-ui/luibackend/pages.py
+++ apps/nspanel-lovelace-ui/luibackend/pages.py
@@ -34,7 +34,7 @@ class LuiPagesGen(object):
                 color = rgb_brightness(color, attr.brightness)
             icon_color = rgb_dec565(color)
         elif "brightness" in attr:
-            color = rgb_brightness([253, 216, 53], attr.brightness)
+            color = rgb_brightness([253, 216, 53], max(70, attr.brightness))
             icon_color = rgb_dec565(color)
         return icon_color
 
