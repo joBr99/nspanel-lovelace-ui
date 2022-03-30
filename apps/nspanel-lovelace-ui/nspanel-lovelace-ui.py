184662d43e1309a2f48b9e04e03cf745c1ab03fa
Johannes <johannes+develop+github@braun-rheingau.de>
2022-03-30 12:28:42 +0200

    bump des vers

--- apps/nspanel-lovelace-ui/nspanel-lovelace-ui.py
+++ apps/nspanel-lovelace-ui/nspanel-lovelace-ui.py
@@ -64,7 +64,7 @@ class NsPanelLovelaceUIManager(hass.Hass):
 
         controller = LuiController(self, cfg, send_mqtt_msg)
 
-        desired_display_firmware_version = 22
+        desired_display_firmware_version = 23
         version     = "v2.1.0"
         
         model       = cfg.get("model")
