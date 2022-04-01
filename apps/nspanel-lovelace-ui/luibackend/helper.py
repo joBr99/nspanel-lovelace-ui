c7dc5d7746855058abe394409b71bd39f2d14278
joBr99 <29555657+joBr99@users.noreply.github.com>
2022-04-01 18:56:03 +0200

    implemented conversion of temperature

--- apps/nspanel-lovelace-ui/luibackend/helper.py
+++ apps/nspanel-lovelace-ui/luibackend/helper.py
@@ -43,6 +43,13 @@ def rgb_dec565(rgb_color):
     # and shift them to make them a 16 bit dec value in 565 format. 
     return ((int(red / 255 * 31) << 11) | (int(green / 255 * 63) << 5) | (int(blue / 255 * 31)))
 
+def convert_temperature_from_celsius(c, unit):
+    if unit == "celsius":
+        temp = (c * 1.8) + 32
+        return f"{temp}°F"
+    else:
+        return f"{temp}°C"
+    
 def get_attr_safe(entity, attr, default):
     res = entity.attributes.get(attr, default)
     if res is None:
