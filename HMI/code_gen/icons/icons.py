2bd12c1d6ca29b9f76d033fce0fca68785108696
Johannes <johannes@braun-rheingau.de>
2022-04-04 21:02:25 +0200

    update icons

--- HMI/code_gen/icons/icons.py
+++ HMI/code_gen/icons/icons.py
@@ -26,49 +26,15 @@ def get_icon_id(ma_name):
         return icons["alert-circle-outline"]
     """)
 
-#icon_nextion_string = ""
-#icon_name_list = []
-#
-#for icon_name in icons:
-#    #print(icon_name)
-#    icon = next((item for item in icon_metadata if item["name"] == icon_name), None)
-#    #if icon is None:
-#    #    print(f"WARNING ICON NOT FOUND: {icon_name}")
-#    #else:
-#    #    hex = icon['hex']
-#    #    s = int(hex, 16)
-#    #    #print(chr(s), end = '')
-#    #    icon_nextion_string += chr(s)
-#    #    icon_name_list.append(icon_name)
-#
-## write mapping lib for python
-#with open(os.path.join(__location__, "../../../apps/nspanel-lovelace-ui/luibackend", "icon_mapping.py"), 'w') as f:
-#    f.write("icons = {\n")
-#    for idx, val in enumerate(icon_name_list):
-#        f.write(f"    '{val}': {idx},\n")
-#    f.write("}\n")
-#    f.write("""
-#def get_icon_id(ma_name):
-#    if ma_name in icons:
-#        return icons[ma_name]
-#    else:
-#        return icons["alert-circle-outline"]
-#    """)
-#
-## write documentation file
-#with open(os.path.join(__location__, "../..","icons.md"), 'w') as f:
-#    f.write("""
-## Icons IDs
-#This file contains the Icons IDs included in the display firmware, addressable via serial.
-#
-#ID | MD Icon Name | Icon
-#-- | ------------ | ----
-#""")
-#    for idx, val in enumerate(icon_name_list):
-#        f.write(f"{idx} | {val} | ![{val}](https://raw.githubusercontent.com/Templarian/MaterialDesign-SVG/0aeb4d612644d80d9d1fe242f705f362985de5dc/svg/{val}.svg)\n")
-#
-#
-#print("=== STRING for HMI Project ===")
-#print("=== Put the following string into the txt field in nextion ===")
-#print(icon_nextion_string)
-#
+# write documentation file
+with open(os.path.join(__location__, "../..","icons.md"), 'w') as f:
+    f.write("""
+# Icons IDs
+This file contains the Icons IDs included in the display firmware, addressable via serial.
+
+MD Icon Name | Icon
+------------ | ----
+""")
+    for icon in icon_metadata:
+        val = icon["name"]
+        f.write(f"{val} | ![{val}](https://raw.githubusercontent.com/Templarian/MaterialDesign-SVG/0aeb4d612644d80d9d1fe242f705f362985de5dc/svg/{val}.svg)\n")
