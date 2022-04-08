d3314736a51754c0288f14efb558012bf0902ba1
joBr99 <29555657+joBr99@users.noreply.github.com>
2022-04-08 22:34:21 +0200

    bumped version

--- apps/nspanel-lovelace-ui/nspanel-lovelace-ui.py
+++ apps/nspanel-lovelace-ui/nspanel-lovelace-ui.py
@@ -63,8 +63,8 @@ class NsPanelLovelaceUIManager(hass.Hass):
 
         controller = LuiController(self, cfg, send_mqtt_msg)
         
-        desired_display_firmware_version = 29
-        version     = "v2.5.5"
+        desired_display_firmware_version = 30
+        version     = "v2.6.0"
         
         model       = cfg.get("model")
         if model == "us-l":
