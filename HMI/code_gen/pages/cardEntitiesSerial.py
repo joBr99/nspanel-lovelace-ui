72314db0020758a5e1e07e6fd5f35f5c88a8ff9a
jmbinette <jeanmichel.binette@gmail.com>
2022-04-12 12:14:57 -0400

    Changed entities layout for 5 only

--- HMI/code_gen/pages/cardEntitiesSerial.py
+++ HMI/code_gen/pages/cardEntitiesSerial.py
@@ -7,7 +7,7 @@ head = sharedhead + """
 """ + navigation
 print(head)
 start = 3
-for i in range(1,7):
+for i in range(1,6):
     idxstart = start + (i-1)*6
     item = f"""
               // get Type
