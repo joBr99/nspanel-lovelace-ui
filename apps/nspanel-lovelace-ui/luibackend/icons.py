08b5024da488c09f46a6c31126e721653b17bf4b
Johannes <johannes@braun-rheingau.de>
2022-05-05 21:21:06 +0200

    Update icons.py

--- apps/nspanel-lovelace-ui/luibackend/icons.py
+++ apps/nspanel-lovelace-ui/luibackend/icons.py
@@ -51,6 +51,8 @@ def map_to_mdi_name(ha_type, state=None, device_class=None):
         return "check-circle-outline" if state == "on" else "close-circle-outline"
     if ha_type == "cover":
         return "window-open" if state == "open" else "window-closed"
+    if ha_type == "lock":
+        return "lock-open" if state == "open" else "lock"
 
     elif ha_type == "sensor":
         if state == "on":
