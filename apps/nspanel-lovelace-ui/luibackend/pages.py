60340123ceade60a8f3589e9d66b51ec353c9eaa
Johannes <johannes@braun-rheingau.de>
2022-05-28 10:23:14 +0200

    disaplay state and hvac action

--- apps/nspanel-lovelace-ui/luibackend/pages.py
+++ apps/nspanel-lovelace-ui/luibackend/pages.py
@@ -281,7 +281,7 @@ class LuiPagesGen(object):
             current_temp = get_attr_safe(entity, "current_temperature", "")
             dest_temp    = int(get_attr_safe(entity, "temperature", 0)*10)
             status       = get_attr_safe(entity, "hvac_action", "")
-            status       = get_translation(self._locale,status)
+            state_value  = f"{get_translation(self._locale,status)} ({get_translation(self._locale,entity.state)})"
             min_temp     = int(get_attr_safe(entity, "min_temp", 0)*10)
             max_temp     = int(get_attr_safe(entity, "max_temp", 0)*10)
             step_temp    = int(get_attr_safe(entity, "target_temp_step", 0.5)*10) 
@@ -321,7 +321,7 @@ class LuiPagesGen(object):
             state_translation = get_translation(self._locale, "state")
             action_translation = get_translation(self._locale, "operation")
 
-            command = f"entityUpd~{heading}~{navigation}~{item}~{current_temp} {temperature_unit}~{dest_temp}~{status}~{min_temp}~{max_temp}~{step_temp}{icon_res}~{currently_translation}~{state_translation}~{action_translation}~{temperature_unit_icon}"
+            command = f"entityUpd~{heading}~{navigation}~{item}~{current_temp} {temperature_unit}~{dest_temp}~{state_value}~{min_temp}~{max_temp}~{step_temp}{icon_res}~{currently_translation}~{state_translation}~{action_translation}~{temperature_unit_icon}"
         self._send_mqtt_msg(command)
 
     def generate_media_page(self, navigation, title, entity):
