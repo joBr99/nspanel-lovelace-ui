59218c1303c9be09d221cf71cbe3887ed7f38959
illuzn <57167030+illuzn@users.noreply.github.com>
2022-06-21 17:51:52 +0930

    Wrong code committed - my bad

--- apps/nspanel-lovelace-ui/luibackend/pages.py
+++ apps/nspanel-lovelace-ui/luibackend/pages.py
@@ -166,7 +166,8 @@ class LuiPagesGen(object):
             page_search_res = self._config.searchCard(entityId)
             if page_search_res is not None:
                 icon_res = get_icon_id("gesture-tap-button")
-                icon_res = get_icon_id(icon) if icon is not None and type(icon) is not dict
+                if icon is not None and type(icon) is not dict:
+                    icon_res = get_icon_id(icon)
                 status_entity = None
                 name = name if name is not None else page_search_res.title
                 text = get_translation(self._locale, "frontend.ui.card.button.press")
