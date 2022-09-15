83f79bf6cde8a74a3215a9fc6af1ab68dec94d16
Johannes <johannes@braun-rheingau.de>
2022-09-15 19:38:47 +0200

    pass ha_type

--- apps/nspanel-lovelace-ui/luibackend/pages.py
+++ apps/nspanel-lovelace-ui/luibackend/pages.py
@@ -190,7 +190,7 @@ class LuiPagesGen(object):
                 entity = apis.ha_api.get_entity(statusIcon.get("entity"))
                 entityType = statusIcon.get("entity").split(".")[0]
                 icon = get_icon(entityType, state=entity.state, device_class=entity.attributes.get("device_class", ""), overwrite=icon)
-                color = self.get_entity_color(entity, overwrite=statusIcon.get("color", None))
+                color = self.get_entity_color(entity, ha_type=entityType, overwrite=statusIcon.get("color", None))
                 status_res += f"~{icon}~{color}"
 
         self._send_mqtt_msg(f"weatherUpdate~{icon_cur}~{text_cur}{weather_res}{altLayout}{status_res}")
