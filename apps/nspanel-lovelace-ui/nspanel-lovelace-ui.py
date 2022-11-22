6aade7ddcd422ac2d675e8b67739856b84610000
Johannes <johannes+develop+github@braun-rheingau.de>
2022-11-22 20:08:37 +0100

    Bump to Version 3.6.0 and Firmware 45

--- apps/nspanel-lovelace-ui/nspanel-lovelace-ui.py
+++ apps/nspanel-lovelace-ui/nspanel-lovelace-ui.py
@@ -20,8 +20,8 @@ class NsPanelLovelaceUIManager(hass.Hass):
 
         controller = LuiController(cfg, mqttsend.send_mqtt_msg)
         
-        desired_display_firmware_version = 43
-        version     = "v3.5.0"
+        desired_display_firmware_version = 45
+        version     = "v3.6.0"
         
         model       = cfg.get("model")
         if model == "us-l":
