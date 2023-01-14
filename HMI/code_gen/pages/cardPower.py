a8c32c35427035ac1b161e113fd5c18c0b7893e8
Johannes Braun <johannes@braun-rheingau.de>
2023-01-14 14:46:46 +0100

    some final cleanup of power page

--- HMI/code_gen/pages/cardPower.py
+++ HMI/code_gen/pages/cardPower.py
@@ -6,15 +6,25 @@ head = sharedhead + """
             spstr strCommand.txt,tHeading.txt,"~",1
 """ + navigation + """
 			  // icon color home
-              spstr strCommand.txt,tTmp.txt,"~",14
-              covx tTmp.txt,t1.pco,0,0
-			        // icon home
-              spstr strCommand.txt,t1.txt,"~",15
-			        // text home
-              spstr strCommand.txt,tHome.txt,"~",16
+        spstr strCommand.txt,tTmp.txt,"~",14
+        covx tTmp.txt,t1.pco,0,0
+			  // icon home
+        spstr strCommand.txt,t1.txt,"~",15
+        // speed 16 ignored
+			  // text home
+        spstr strCommand.txt,tHome.txt,"~",17
+        spstr tHome.txt,tHome2.txt," ",1
+        spstr tHome.txt,tHome.txt," ",0
+        // icon color 18 ignored
+        // icon 19 ignored
+        // speed 20 ignored
+			  // text home
+        spstr strCommand.txt,tHomeO.txt,"~",21
+        spstr tHomeO.txt,tHomeO2.txt," ",1
+        spstr tHomeO.txt,tHomeO.txt," ",0
 """
 print(head)
-start = 17
+start = 22
 for i in range(0,6):
     idxstart = start + (i)*4
     item = f"""
