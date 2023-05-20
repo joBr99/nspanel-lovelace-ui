31523f99a068be2ffd240b5c3137603ba4f72666
Johannes <29555657+joBr99@users.noreply.github.com>
2023-05-20 17:16:22 +0200

    Update nspanel-lovelace-ui.py

--- apps/nspanel-lovelace-ui/nspanel-lovelace-ui.py
+++ apps/nspanel-lovelace-ui/nspanel-lovelace-ui.py
@@ -27,8 +27,8 @@ class NsPanelLovelaceUIManager(hass.Hass):
         controller = LuiController(cfg, mqttsend.send_mqtt_msg)
         
         desired_tasmota_driver_version   = 8
-        desired_display_firmware_version = 50
-        version     = "v4.0.3"
+        desired_display_firmware_version = 51
+        version     = "v4.1.1"
         
         model       = cfg.get("model")
         if model == "us-l":
