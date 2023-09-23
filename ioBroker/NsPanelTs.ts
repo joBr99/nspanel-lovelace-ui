df105a22333d43fef931293d5904cca806983c9d
Armilar <102996011+Armilar@users.noreply.github.com>
2023-09-23 21:13:52 +0200

    v4.3.1.3 - Update BerryDriver / Service Menu Fix
    v4.3.1.2  Upgrade BerryDriver v9
    v4.3.1.3  Fix - Change ServivceMenu from Fake-SSId to real Tasmota-SSIdParam
     
    Todo:
    v4.4.0    Change the bottomScreensaverEntity (rolling) if more than 6 entries are defined

--- ioBroker/NsPanelTs.ts
+++ ioBroker/NsPanelTs.ts
@@ -1,5 +1,5 @@
 /*-----------------------------------------------------------------------
-TypeScript v4.3.1.2 zur Steuerung des SONOFF NSPanel mit dem ioBroker by @Armilar / @TT-Tom / @Sternmiere / @Britzelpuf / @ravenS0ne
+TypeScript v4.3.1.3 zur Steuerung des SONOFF NSPanel mit dem ioBroker by @Armilar / @TT-Tom / @Sternmiere / @Britzelpuf / @ravenS0ne
 - abgestimmt auf TFT 53 / v4.3.1 / BerryDriver 9 / Tasmota 13.1.0
 @joBr99 Projekt: https://github.com/joBr99/nspanel-lovelace-ui/tree/main/ioBroker
 NsPanelTs.ts (dieses TypeScript in ioBroker) Stable: https://github.com/joBr99/nspanel-lovelace-ui/blob/main/ioBroker/NsPanelTs.ts
