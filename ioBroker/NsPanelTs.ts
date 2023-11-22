32df87d8bd96e19bc636932c6021d10302378b89
Armilar <102996011+Armilar@users.noreply.github.com>
2023-11-22 19:40:51 +0100

    v4.3.3.10 - Update NsPanelTs.ts
    Code optimization in Config Area

--- ioBroker/NsPanelTs.ts
+++ ioBroker/NsPanelTs.ts
@@ -59,7 +59,7 @@ ReleaseNotes:
         - 20.11.2023 - v4.3.3.6  Add Multilingualism to cardAlarm (39 languages)
         - 20.11.2023 - v4.3.3.7  Add Multilingualism to cardMedia (39 languages)
         - 20.11.2023 - v4.3.3.8  Add Method dayjs (Multilingualism), some Minor Fixes
-	    - 20.11.2023 - v4.3.3.9  Add ScreensaverEntityOnColor, ...OffColor, ...OnText, ...OffText
+	- 20.11.2023 - v4.3.3.9  Add ScreensaverEntityOnColor, ...OffColor, ...OnText, ...OffText
         - 21.11.2023 - v4.3.3.10 Code optimization
         
         Todo:
@@ -864,7 +864,7 @@ export const config = <Config> {
         page: null,     // Zielpage - Verwendet wenn mode = page (bisher button2Page)
         entity: null,   // Zielentity - Verwendet wenn mode = set oder toggle
         setValue: null  // Zielwert - Verwendet wenn mode = set
-    }
+    },
 //--------- Ende   Einstellungen für Hardware Button, wenn Sie softwareseitig genutzt werden (Rule2) -------------
 
 
