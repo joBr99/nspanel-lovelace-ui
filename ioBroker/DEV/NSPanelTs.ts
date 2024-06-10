63c85e6b9616fe54c395020cf2ea332d907903e6
Thomas <101348966+tt-tom17@users.noreply.github.com>
2024-06-10 22:31:36 +0200

    Update NSPanelTs.ts
    Fix TFT-Version

--- ioBroker/DEV/NSPanelTs.ts
+++ ioBroker/DEV/NSPanelTs.ts
@@ -1,6 +1,6 @@
 /*-----------------------------------------------------------------------
-TypeScript v4.4.0.0 zur Steuerung des SONOFF NSPanel mit dem ioBroker by @Armilar / @TT-Tom / @ticaki / @Britzelpuf / @Sternmiere / @ravenS0ne
-- abgestimmt auf TFT 54 / v4.4.0 / BerryDriver 9 / Tasmota 13.4.0
+TypeScript v4.4.0.1 zur Steuerung des SONOFF NSPanel mit dem ioBroker by @Armilar / @TT-Tom / @ticaki / @Britzelpuf / @Sternmiere / @ravenS0ne
+- abgestimmt auf TFT 53 / v4.4.0 / BerryDriver 9 / Tasmota 13.4.0
 @joBr99 Projekt: https://github.com/joBr99/nspanel-lovelace-ui/tree/main/ioBroker
 NsPanelTs.ts (dieses TypeScript in ioBroker) Stable: https://github.com/joBr99/nspanel-lovelace-ui/blob/main/ioBroker/NsPanelTs.ts
 icon_mapping.ts: https://github.com/joBr99/nspanel-lovelace-ui/blob/main/ioBroker/icon_mapping.ts (TypeScript muss in global liegen)
@@ -119,6 +119,7 @@ ReleaseNotes:
 	- 11.02.2024 - v4.3.3.43 Fix VolumeSlider
         - 05.05.2024 - v4.3.3.44 Fix MQTT-Port-check
 	- 13.05.2024 - v4.4.0.0  TFT 54 / 4.4.0
+ 	- 10.06.2024 - v4.4.0.1  TFT 53 / 4.4.0
 
         Todo:
         - XX.XX.XXXX - v5.0.0    Change the bottomScreensaverEntity (rolling) if more than 6 entries are defined	
@@ -987,9 +988,9 @@ export const config: Config = {
 // _________________________________ DE: Ab hier keine Konfiguration mehr _____________________________________
 // _________________________________ EN:  No more configuration from here _____________________________________
 
-const scriptVersion: string = 'v4.4.0.0';
+const scriptVersion: string = 'v4.4.0.1';
 const tft_version: string = 'v4.4.0';
-const desired_display_firmware_version = 54;
+const desired_display_firmware_version = 53;
 const berry_driver_version = 9;
 
 const tasmotaOtaUrl: string = 'http://ota.tasmota.com/tasmota32/release/';
