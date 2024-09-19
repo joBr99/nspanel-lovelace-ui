104788e2a473f830f4f0d10c9c8953df368ee4de
Armilar <102996011+Armilar@users.noreply.github.com>
2024-09-19 22:46:35 +0200

    v4.4.0.6 - DEV Update NSPanelTs.ts
    - Check Ports with mqtt.X and mqtt-client.X

--- ioBroker/DEV/NSPanelTs.ts
+++ ioBroker/DEV/NSPanelTs.ts
@@ -1,5 +1,5 @@
 /*-----------------------------------------------------------------------
-TypeScript v4.4.0.5 zur Steuerung des SONOFF NSPanel mit dem ioBroker by @Armilar / @TT-Tom / @ticaki / @Britzelpuf / @Sternmiere / @ravenS0ne
+TypeScript v4.4.0.6 zur Steuerung des SONOFF NSPanel mit dem ioBroker by @Armilar / @TT-Tom / @ticaki / @Britzelpuf / @Sternmiere / @ravenS0ne
 - abgestimmt auf TFT 53 / v4.4.0 / BerryDriver 9 / Tasmota 14.2.0
 @joBr99 Projekt: https://github.com/joBr99/nspanel-lovelace-ui/tree/main/ioBroker
 NsPanelTs.ts (dieses TypeScript in ioBroker) Stable: https://github.com/joBr99/nspanel-lovelace-ui/blob/main/ioBroker/NsPanelTs.ts
@@ -124,6 +124,7 @@ ReleaseNotes:
         - 13.06.2024 - v4.4.0.3  Check prefix '.tele.' in config.NSPanelReceiveTopic
         - 13.09.2024 - v4.4.0.4  New Feature: Hidden Carts
         - 18.09.2024 - v4.4.0.5  Remove day.JS
+	- 19.09.2024 - v4.4.0.6  Check Ports with mqtt.X and mqtt-client.X
 
         Todo:
         - XX.12.2024 - v5.0.0    ioBroker Adapter
@@ -1199,7 +1200,7 @@ CheckDebugMode();
 
 async function CheckMQTTPorts() {
     try {
-        let instanceName: string = config.panelRecvTopic.substring(0, 6);
+        let instanceName: string = (config.panelRecvTopic).split('.')[0] + "." + (config.panelRecvTopic).split('.')[1];
 
         if (isSetOptionActive) {
             await createStateAsync(NSPanel_Path + 'Config.MQTT.portCheck', true, { type: 'boolean', write: true });
